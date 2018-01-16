b2ce2dad8528db0276ac6e8e5e260ac8e37611b4
kevienjun <h1253516666@gmail.com>
2018-01-16 14:32:37 +0800

    exam

---
+++ exam/src/edu/fju/exam/Niner.java
@@ -0,0 +1,17 @@
+package edu.fju.exam;
+
+public class Niner {
+
+	public static void main(String[] args) {
+
+		for (int n = 1; n <= 5; n++) {
+			for (int i = 2; i <=5 ; i++) {
+				System.out.print(i + " x " + n + " = " + (n * i) + "\t");
+
+			}
+
+			System.out.println();
+		}
+
+	}
+}
